cd3ba5251011f47b2bf2998bfd218b6424213009
DimitriPalmer22 <dimitriPalmer22@gmail.com>
2025-01-26 23:22:25 -0500

    Game BGM

---
+++ Assets/_Scripts/Managers/Sound Management/Sound.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using UnityEngine;
+
+[Serializable]
+public class Sound
+{
+    #region Serialized Fields
+
+    [SerializeField] private AudioClip clip;
+
+    [SerializeField] private SoundType soundType = SoundType.PlayerSfx;
+
+    [SerializeField] [Range(0, 1)] private float volume = 1;
+    [SerializeField] private bool isPersistent;
+    [SerializeField] private bool isLooping;
+    [SerializeField, Range(.01f, 2f)] private float pitch = 1;
+
+    #endregion
+
+    #region Getters
+
+    public AudioClip Clip => clip;
+
+    public SoundType SoundType => soundType;
+
+
+    public bool IsPersistent => isPersistent;
+
+    public float Volume => volume;
+
+    public bool IsLooping => isLooping;
+    
+    public float Pitch => pitch;
+
+    #endregion
+
+    public override string ToString()
+    {
+        if (clip == null)
+            return "";
+
+        return $"[Clip: {clip?.name}, Type: {soundType}]";
+    }
+}
---
+++ Assets/_Scripts/Managers/Sound Management/SoundPool.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+[Serializable]
+public class SoundPool
+{
+    [SerializeField] private Sound[] sounds;
+
+    public IReadOnlyCollection<Sound> Sounds => sounds;
+
+    public Sound GetRandomSound()
+    {
+        // Avoid null reference exception
+        if (sounds == null || sounds.Length == 0)
+            return null;
+
+        // Get the index of the random sound
+        var randomIndex = UnityEngine.Random.Range(0, sounds.Length);
+
+        return sounds[randomIndex];
+    }
+}
