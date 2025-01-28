2cffcfd58dc66a16ff6d4e7bbc5055d7a2ad98ad
Andre Powell <andre.r.powell@knights.ucf.edu>
2025-01-28 06:55:54 -0500

    Made Separate Controllers

--- Assets/AnimationHandler.cs
+++ Assets/AnimationHandler.cs
@@ -1,11 +1,20 @@
+using System.Collections;
 using UnityEngine;
 
 public class AnimationHandler : StateMachineBehaviour
 {
+   //static variable to store the hash of the idle parameter
+   int idleHash = Animator.StringToHash("IdleNum");
    public override void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
    {
-      //static variable to store the hash of the idle parameter
-      int idleHash = Animator.StringToHash("IdleNum");
+      // Start the coroutine using a helper MonoBehaviour
+      animator.GetComponent<AnimationHelperScript>()?.StartCoroutine(Wait(animator));
+   }
+   
+   //Wait for 10 seconds before changing the idle animation
+   IEnumerator Wait(Animator animator)
+   {
+      yield return new WaitForSeconds(10);
       
       int randomIdle = Random.Range(1, 4);
       
