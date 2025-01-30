732f1da92e360d5a313d8b3d7bce0d5092b35325
Andre Powell <andre.r.powell@knights.ucf.edu>
2025-01-29 19:27:13 -0500

    Added sound effects to menu buttons

---
+++ Assets/Hoversound.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+
+public class Hoversound : MonoBehaviour, IPointerEnterHandler
+{
+    [SerializeField]private AudioClip hoverSound;
+    [SerializeField]private AudioSource source;
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        // Play the hover sound
+        source.PlayOneShot(hoverSound);
+        Debug.Log("Mouse entered");
+        
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        Debug.Log("Mouse exited");
+    }
+}
+
+
