9198d7380a486a214271af0556c28e05001be78d
DimitriPalmer22 <dimitriPalmer22@gmail.com>
2025-01-30 22:25:25 -0500

    Epilepsy Warning

---
+++ Assets/_Scripts/UI/Menus/GenericMenu.cs
@@ -0,0 +1,30 @@
+﻿public class GenericMenu : GameMenu
+{
+    protected override void CustomAwake()
+    {
+    }
+
+    protected override void CustomStart()
+    {
+    }
+
+    protected override void CustomDestroy()
+    {
+    }
+
+    protected override void CustomActivate()
+    {
+    }
+
+    protected override void CustomDeactivate()
+    {
+    }
+
+    protected override void CustomUpdate()
+    {
+    }
+
+    public override void OnBackPressed()
+    {
+    }
+}
