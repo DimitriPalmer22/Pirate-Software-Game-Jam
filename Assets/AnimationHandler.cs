ac32ddd7a2e8be2b0d13b7def6fd16c76c4db880
Andre Powell <andre.r.powell@knights.ucf.edu>
2025-01-31 03:00:03 -0500

    fixed PlayerModel readded Animations

--- Assets/AnimationHandler.cs
+++ Assets/AnimationHandler.cs
@@ -3,24 +3,43 @@ using UnityEngine;
 
 public class AnimationHandler : StateMachineBehaviour
 {
-   //static variable to store the hash of the idle parameter
-   int idleHash = Animator.StringToHash("IdleNum");
-   public override void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
-   {
-      // Start the coroutine using a helper MonoBehaviour
-      animator.GetComponent<AnimationHelperScript>()?.StartCoroutine(Wait(animator));
-   }
-   
-   //Wait for 10 seconds before changing the idle animation
-   IEnumerator Wait(Animator animator)
-   {
-      yield return new WaitForSeconds(10);
-      
-      int randomIdle = Random.Range(1, 4);
-      
-      //set idle parameter to random number
-      animator.SetInteger(idleHash, randomIdle);
-   }
+    private int idleHash = Animator.StringToHash("IdleNum");
+    private Coroutine _idleRoutine;
+    private AnimationHelperScript _helper;
+    [SerializeField] private float idleTime = 10f;
+
+    public override void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        // Get or create the helper component
+        _helper = animator.GetComponent<AnimationHelperScript>();
+        if (_helper == null) 
+        {
+            _helper = animator.gameObject.AddComponent<AnimationHelperScript>();
+        }
+
+        // Start the looping coroutine
+        _idleRoutine = _helper.StartCoroutine(IdleCycle(animator));
+    }
+
+    public override void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        // Stop the coroutine when leaving the state
+        if (_idleRoutine != null && _helper != null)
+        {
+            _helper.StopCoroutine(_idleRoutine);
+        }
+    }
+
+    IEnumerator IdleCycle(Animator animator)
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(idleTime);
+            
+            int randomIdle = Random.Range(1, 4);
+            animator.SetInteger(idleHash, randomIdle);
+        }
+    }
 }
 
 
